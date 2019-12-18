ad79b753ee0887415c6a16de3e3e29a8c88bd0c4
marek sitarski <marek_sitarski@vp.pl>
2019-12-18 22:46:27 +0100

    SendTruckDriverPositionAndDataService refractorization

--- app/src/main/java/com/example/truckpark/service/positionsender/SendTruckDriverPositionAndDataService.java
+++ app/src/main/java/com/example/truckpark/service/positionsender/SendTruckDriverPositionAndDataService.java
@@ -1,25 +1,34 @@
 package com.example.truckpark.service.positionsender;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
-import android.os.StrictMode;
 
+import com.example.truckpark.properties.PropertyManager;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDateTime;
 
 public class SendTruckDriverPositionAndDataService extends Service {
 
+    private final String APIKEY;
+    private final String URI;
+
     private final IBinder binder = new SendTruckDriverPositionAndDataBinder();
 
-    public SendTruckDriverPositionAndDataService() {
+    public SendTruckDriverPositionAndDataService(Context context) {
+        PropertyManager propertyManager = new PropertyManager("truckparkserver.properties");
+        APIKEY= propertyManager.getProperty("APIKEY", context);
+        URI= propertyManager.getProperty("URI", context);
     }
-
 
     public class SendTruckDriverPositionAndDataBinder extends Binder {
         public SendTruckDriverPositionAndDataService getSendTruckDriverPositionAndData() {
@@ -40,47 +49,68 @@ public class SendTruckDriverPositionAndDataService extends Service {
     public void sendPost() {
         Thread thread = new Thread(() -> {
             try {
-                //////////////////////////
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-                /////////////////////////
-                URL url = new URL("http://192.168.0.21:8080/rest/api/truckdriverways/truckdriverway");
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-                conn.setRequestProperty("Accept", "application/json");
-                conn.setDoOutput(true);
-                conn.setDoInput(true);
 
-                LocalDateTime fromDateAndTime = LocalDateTime.now();
+                String httpAdress = buildUrl();
+                URL url = new URL(httpAdress);
+                HttpURLConnection connectionToRest = getHttpURLConnection(url);
 
-                JSONObject truckDriverWayJSON = new JSONObject();
+                JSONObject truckDriverWayJSON = createTruckDriverWayJson();
+                DataOutputStream jsonOutputStream = new DataOutputStream(connectionToRest.getOutputStream());
+                String truckDriverWayJSONAsString = truckDriverWayJSON.toString();
+                jsonOutputStream.writeBytes(truckDriverWayJSONAsString);
+                String.valueOf(connectionToRest.getResponseCode());
 
-                JSONObject coordinateJSON = new JSONObject();
-                coordinateJSON.put("id", null);
-                coordinateJSON.put("x", 21.1);
-                coordinateJSON.put("y", 52.3);
+                jsonOutputStream.flush();
+                connectionToRest.disconnect();
 
-
-                truckDriverWayJSON.put("id", null);
-                truckDriverWayJSON.put("resultTime", LocalDateTime.now());
-                truckDriverWayJSON.put("distance", 21.2);
-                truckDriverWayJSON.put("fuel", 2.54);
-                truckDriverWayJSON.put("driverId", 1);
-                truckDriverWayJSON.put("truckId", 1);
-                truckDriverWayJSON.put("coordinateDto", coordinateJSON);
-
-                DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-                String string = truckDriverWayJSON.toString();
-                os.writeBytes(string);
-                os.flush();
-                os.close();
-                String.valueOf(conn.getResponseCode());
-                conn.disconnect();
-            } catch (Exception e) {
-                e.printStackTrace();
+            } catch (IOException ioexception) {
+                ioexception.printStackTrace();
+            } catch (JSONException jsone) {
+                jsone.printStackTrace();
             }
         });
         thread.start();
+    }
+
+    private String buildUrl(){
+        StringBuilder buildedURL = new StringBuilder();
+        buildedURL.append(URI);
+        buildedURL.append("/");
+        buildedURL.append("truckdriverways");
+        buildedURL.append("/");
+        buildedURL.append("truckdriverway");
+
+        return buildedURL.toString();
+    }
+
+    private HttpURLConnection getHttpURLConnection(URL url) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+        return conn;
+    }
+
+    private JSONObject createTruckDriverWayJson() throws JSONException {
+
+        JSONObject coordinateJSON = new JSONObject();
+
+        coordinateJSON.put("id", null);
+        coordinateJSON.put("x", 21.1);
+        coordinateJSON.put("y", 52.3);
+
+        JSONObject truckDriverWayJSON = new JSONObject();
+
+        truckDriverWayJSON.put("id", null);
+        truckDriverWayJSON.put("resultTime", LocalDateTime.now());
+        truckDriverWayJSON.put("distance", 21.2);
+        truckDriverWayJSON.put("fuel", 2.54);
+        truckDriverWayJSON.put("driverId", 1);
+        truckDriverWayJSON.put("truckId", 1);
+        truckDriverWayJSON.put("coordinateDto", coordinateJSON);
+
+        return truckDriverWayJSON;
     }
 }
