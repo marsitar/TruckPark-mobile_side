c037908b2d7b83173829f5281be440c6a7b8a0c4
marek sitarski <marek_sitarski@vp.pl>
2020-01-14 23:58:18 +0100

    Complete tests of SimpleRouteServiceTest class and fix tests in GoogleRouteRequestAsyncTaskTest. Do some refractorization in SimpleRouteService.

--- app/src/main/java/com/example/truckpark/service/route/SimpleRouteService.java
+++ app/src/main/java/com/example/truckpark/service/route/SimpleRouteService.java
@@ -4,12 +4,16 @@ import android.content.Context;
 import android.util.Log;
 
 import com.example.truckpark.domain.json.googledirectionsapi.GoogleRoute;
-import com.example.truckpark.domain.json.googledirectionsapi.LatLng;
+import com.example.truckpark.domain.json.googledirectionsapi.Leg;
+import com.example.truckpark.domain.json.googledirectionsapi.Polyline;
+import com.example.truckpark.domain.json.googledirectionsapi.Route;
 import com.example.truckpark.domain.json.googledirectionsapi.Step;
 import com.google.maps.android.PolyUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class SimpleRouteService {
 
@@ -23,38 +27,59 @@ public class SimpleRouteService {
 
         GoogleRoute googleRoute = googleRouteService.getGoogleRoute(origin, destination);
 
-        List<Step> steps = googleRoute.getRoutes().get(0).getLegs().get(0).getSteps();
+        List<Step> steps = Optional.ofNullable(googleRoute)
+                .map(GoogleRoute::getRoutes)
+                .orElseGet(Collections::emptyList)
+                .stream()
+                .findFirst()
+                .map(Route::getLegs)
+                .orElseGet(Collections::emptyList)
+                .stream()
+                .findFirst()
+                .map(Leg::getSteps)
+                .orElseGet(Collections::emptyList);
 
-        fillPointsList(points, steps);
+        fillPointsList(steps, points);
 
         Log.d(className, String.format("Simple route has been created with current data from GoogleMaps. Origin=%s, destination=%s", origin, destination));
 
         return points;
     }
 
-    private void fillPointsList(List<Double[]> points, List<Step> steps) {
+    private void fillPointsList(List<Step> steps, List<Double[]> points) {
 
-        steps.forEach(step -> {
-            Double[] coordinates = new Double[2];
-            coordinates[0] = step.getStartLocation().getLat();
-            coordinates[1] = step.getStartLocation().getLng();
+        Optional.ofNullable(steps)
+            .orElseGet(Collections::emptyList)
+            .forEach(step -> {
+                Double[] coordinates = new Double[2];
+                coordinates[0] = step.getStartLocation().getLat();
+                coordinates[1] = step.getStartLocation().getLng();
 
-            points.add(coordinates);
+                points.add(coordinates);
 
-            List<com.google.android.gms.maps.model.LatLng> insideStepPolylinePoints = PolyUtil.decode(step.getPolyline().getPoints());
+                List<com.google.android.gms.maps.model.LatLng> insideStepPolylinePoints = PolyUtil.decode(
+                        Optional.of(step)
+                                .map(Step::getPolyline)
+                                .map(Polyline::getPoints)
+                                .orElse("")
+                );
 
-            insideStepPolylinePoints.forEach(point -> {
-                Double[] coordinatesInsidePolygon = new Double[2];
-                coordinatesInsidePolygon[0] = point.latitude;
-                coordinatesInsidePolygon[1] = point.longitude;
-                points.add(coordinatesInsidePolygon);
-            });
-
+                insideStepPolylinePoints
+                        .stream()
+                        .map(point -> new Double[]{point.latitude, point.longitude})
+                        .forEach(points::add);
         });
 
-        LatLng endPoint = steps.get(steps.size() - 1).getEndLocation();
-        points.add(new Double[]{endPoint.getLat(), endPoint.getLng()});
+        Optional.ofNullable(steps)
+            .orElseGet(Collections::emptyList)
+            .stream()
+            .reduce((first, second) -> second)
+            .map(Step::getEndLocation)
+            .ifPresent(
+                    latLng -> points.add(
+                            new Double[]{latLng.getLat(), latLng.getLng()})
+            );
 
-        Log.d(className, String.format("Points list has been filled with current data from GoogleMaps. PointsList=%s", points.toArray()));
+        Log.d(className, String.format("Points list has been filled with current data from GoogleMaps. PointsList=%s", points.toString()));
     }
 }
