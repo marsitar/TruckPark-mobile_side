e56926bf4dd97fc0233a55ad5194eb65dc633ce9
marek sitarski <marek_sitarski@vp.pl>
2020-01-23 23:31:30 +0100

    generate RouteSchedule builder

--- app/src/main/java/com/example/truckpark/domain/entity/RouteSchedule.java
+++ app/src/main/java/com/example/truckpark/domain/entity/RouteSchedule.java
@@ -1,4 +1,45 @@
 package com.example.truckpark.domain.entity;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RouteSchedule {
+
+    private LocalDateTime saveDateAndTime;
+    private List<RoutePart> routeParts = new ArrayList<>();
+
+    public static final class Builder {
+
+        private LocalDateTime saveDateAndTime;
+        private List<RoutePart> routeParts = new ArrayList<>();
+
+        public Builder withSaveDateAndTime(LocalDateTime saveDateAndTime) {
+            this.saveDateAndTime = saveDateAndTime;
+            return this;
+        }
+
+        public Builder withRouteParts(List<RoutePart> routeParts) {
+            this.routeParts = routeParts;
+            return this;
+        }
+
+        public RouteSchedule build() {
+
+            RouteSchedule routeSchedule = new RouteSchedule();
+
+            routeSchedule.saveDateAndTime = this.saveDateAndTime;
+            routeSchedule.routeParts = this.routeParts;
+
+            return routeSchedule;
+        }
+    }
+
+    public LocalDateTime getSaveDateAndTime() {
+        return saveDateAndTime;
+    }
+
+    public List<RoutePart> getRouteParts() {
+        return routeParts;
+    }
 }
