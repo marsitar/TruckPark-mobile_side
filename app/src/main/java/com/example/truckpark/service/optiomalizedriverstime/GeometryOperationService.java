2a124a298f79778ca6bb16b46f98825f14ee5b5d
marek sitarski <marek_sitarski@vp.pl>
2020-02-12 22:25:38 +0100

    Create GeometryOperationService contains finding mops within buffer

--- app/src/main/java/com/example/truckpark/service/optiomalizedriverstime/GeometryOperationService.java
+++ app/src/main/java/com/example/truckpark/service/optiomalizedriverstime/GeometryOperationService.java
@@ -1,20 +1,55 @@
 package com.example.truckpark.service.optiomalizedriverstime;
 
+import com.esri.core.geometry.OperatorWithin;
+import com.esri.core.geometry.Point;
+import com.esri.core.geometry.Polygon;
+import com.esri.core.geometry.SpatialReference;
 import com.example.truckpark.domain.json.mopapi.Mop;
+import com.example.truckpark.localdatamanagment.DataGetter;
+import com.example.truckpark.localdatamanagment.MopsDataManagement;
 
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class GeometryOperationService {
 
-    public Set<Mop> getPotentialStopMops() {
+    public List<Mop> getPotentialStopMops(Polygon polygon) {
 
+        List<Mop> mopsFormRepository = getMopsFromRepository();
+
+        List<Mop> potentialStopMops = mopsFormRepository.stream()
+                .filter(mop -> {
+                    Point mopPoint = createArcGisPoint(mop);
+                    return isMopWithinBuffer(mopPoint, polygon);
+                })
+                .collect(Collectors.toList());
+
+        return potentialStopMops;
     }
 
-    private List<Mop> privateGetMopsFromRepository() {
+    private List<Mop> getMopsFromRepository() {
 
+        DataGetter<List<Mop>> mopsDataManagement = new MopsDataManagement();
 
-
-        return null;
+        return mopsDataManagement.getData();
     }
+
+    private Point createArcGisPoint(Mop mop) {
+
+        Double x = mop.getCoordinate().getX();
+        Double y = mop.getCoordinate().getY();
+
+        Point point = new Point();
+        point.setXY(x,y);
+
+        return point;
+    }
+
+    private boolean isMopWithinBuffer(Point mopCoordinates, Polygon buffer) {
+
+        SpatialReference gpsSpatialReference = SpatialReference.create(4326);
+
+        return OperatorWithin.local().execute(mopCoordinates, buffer, gpsSpatialReference, null);
+    }
+
 }
