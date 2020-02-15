45d6e8be26378f903e7ecb30a247fd568e633b8b
marek sitarski <marek_sitarski@vp.pl>
2020-02-15 12:42:58 +0100

    Add loggers to PolylineMessageContentService class

--- app/src/main/java/com/example/truckpark/service/optiomalizedriverstime/PolylineMessageContentService.java
+++ app/src/main/java/com/example/truckpark/service/optiomalizedriverstime/PolylineMessageContentService.java
@@ -1,5 +1,7 @@
 package com.example.truckpark.service.optiomalizedriverstime;
 
+import android.util.Log;
+
 import com.example.truckpark.domain.entity.RouteSchedule;
 import com.example.truckpark.localdatamanagment.DataGetter;
 
@@ -9,39 +11,61 @@ import java.util.Optional;
 
 public class PolylineMessageContentService {
 
+    private final String className = this.getClass().getSimpleName();
+
     public String getRoutePartOrigin(DataGetter<RouteSchedule> routerScheduleDataManagement, int polylineIndex) {
-        return Optional.of(routerScheduleDataManagement)
+
+        String routePartOrigin = Optional.of(routerScheduleDataManagement)
                 .map(DataGetter::getData)
                 .map(RouteSchedule::getRouteParts)
                 .orElseGet(Collections::emptyList)
                 .get(polylineIndex)
                 .getOrigin();
+
+        Log.d(className, String.format("RoutePartOrigin: %s.", routePartOrigin));
+
+        return routePartOrigin;
     }
 
     public String getRoutePartDestination(DataGetter<RouteSchedule> routerScheduleDataManagement, int polylineIndex) {
-        return Optional.of(routerScheduleDataManagement)
+
+        String routePartDestination = Optional.of(routerScheduleDataManagement)
                 .map(DataGetter::getData)
                 .map(RouteSchedule::getRouteParts)
                 .orElseGet(Collections::emptyList)
                 .get(polylineIndex)
                 .getDestination();
+
+        Log.d(className, String.format("RoutePartDestination: %s.", routePartDestination));
+
+        return routePartDestination;
     }
 
     public Duration getRoutePartDuration(DataGetter<RouteSchedule> routerScheduleDataManagement, int polylineIndex) {
-        return Optional.of(routerScheduleDataManagement)
+
+        Duration routePartDuration = Optional.of(routerScheduleDataManagement)
                 .map(DataGetter::getData)
                 .map(RouteSchedule::getRouteParts)
                 .orElseGet(Collections::emptyList)
                 .get(polylineIndex)
                 .getDuration();
+
+        Log.d(className, String.format("RoutePartDuration: %s.", routePartDuration));
+
+        return routePartDuration;
     }
 
     public int getRouteDistance(DataGetter<RouteSchedule> routerScheduleDataManagement, int polylineIndex) {
-        return Optional.of(routerScheduleDataManagement)
+
+        int routeDistance = Optional.of(routerScheduleDataManagement)
                 .map(DataGetter::getData)
                 .map(RouteSchedule::getRouteParts)
                 .orElseGet(Collections::emptyList)
                 .get(polylineIndex)
                 .getDistance();
+
+        Log.d(className, String.format("RouteDistance: %s.", routeDistance));
+
+        return routeDistance;
     }
 }
