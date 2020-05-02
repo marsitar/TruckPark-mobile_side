b77f6dc5f5dd8ca4ecac305548b1f6c030b4b0e5
marek sitarski <marek_sitarski@vp.pl>
2020-05-02 14:34:31 +0200

    First attempt to estabilish KeyCloak authorization and authentication

--- app/src/main/java/com/example/truckpark/view/login/LoginActivity.java
+++ app/src/main/java/com/example/truckpark/view/login/LoginActivity.java
@@ -6,17 +6,26 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.truckpark.view.main.MainMenu;
 import com.example.truckpark.R;
+import com.example.truckpark.view.main.MainMenu;
+
+import org.jboss.aerogear.android.authorization.AuthorizationManager;
+import org.jboss.aerogear.android.authorization.AuthzModule;
+import org.jboss.aerogear.android.authorization.oauth2.OAuth2AuthorizationConfiguration;
+import org.jboss.aerogear.android.core.Callback;
+
+import java.net.URL;
 
 
 public class LoginActivity extends Activity {
 
     private String className = this.getClass().getSimpleName();
+    private AuthzModule authzModule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,47 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_main);
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.authz();
+    }
+
+    private void authz() {
+        try {
+
+            authzModule = AuthorizationManager.config("GoogleDriveAuthz", OAuth2AuthorizationConfiguration.class)
+                    .setBaseURL(new URL("http://192.168.0.20:8091/auth"))
+                    .setAuthzEndpoint("/realms/truckpark/protocol/openid-connect/auth")
+                    .setAccessTokenEndpoint("/realms/truckpark/protocol/openid-connect/token")
+                    .setAccountId("keycloak-token")
+                    .setClientId("truckparkclientfrontend")
+                    .setClientSecret("")
+                    .setRedirectURL("com.example.truckpark.view.main.MainMenu")
+                    .addAdditionalAuthorizationParam((Pair.create("access_type", "offline")))
+                    .asModule();
+
+            authzModule.requestAccess(this, new Callback<String>() {
+                @Override
+                public void onSuccess(String o) {
+                    Log.d("TOKEN ++ ", o);
+                    Toast.makeText(getApplicationContext(), o, Toast.LENGTH_LONG).show();
+
+                }
+
+                @Override
+                public void onFailure(Exception e) {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
