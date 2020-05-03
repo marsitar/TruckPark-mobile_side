aaa59b22369731f8193c537234e0803454594b86
marek sitarski <marek_sitarski@vp.pl>
2020-05-03 19:24:31 +0200

    Add TruckDriverWayDtoCreate classes to build json in proper way

---
+++ app/src/main/java/com/example/truckpark/domain/json/truckdriverwayapi/CoordinateDto.java
@@ -0,0 +1,52 @@
+package com.example.truckpark.domain.json.truckdriverwayapi;
+
+public class CoordinateDto {
+
+    private Long id;
+
+    private Double lat;
+
+    private Double lng;
+
+    public CoordinateDto() {
+    }
+
+    public CoordinateDto(Long id, Double lat, Double lng) {
+        this.id = id;
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+
+    @Override
+    public String toString() {
+        return "CoordinateDto{" +
+                "id=" + id +
+                ", lat=" + lat +
+                ", lng=" + lng +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/truckpark/domain/json/truckdriverwayapi/TruckDriverWayDtoCreate.java
@@ -0,0 +1,76 @@
+package com.example.truckpark.domain.json.truckdriverwayapi;
+
+import java.time.LocalDateTime;
+
+public class TruckDriverWayDtoCreate {
+
+    private Double fuel;
+    private Double distance;
+    private LocalDateTime resultTime;
+    private CoordinateDto coordinateDto;
+    private Long truckId;
+    private Long driverId;
+
+    public TruckDriverWayDtoCreate() {
+    }
+
+    public Double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(Double fuel) {
+        this.fuel = fuel;
+    }
+
+    public Double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Double distance) {
+        this.distance = distance;
+    }
+
+    public LocalDateTime getResultTime() {
+        return resultTime;
+    }
+
+    public void setResultTime(LocalDateTime resultTime) {
+        this.resultTime = resultTime;
+    }
+
+    public CoordinateDto getCoordinateDto() {
+        return coordinateDto;
+    }
+
+    public void setCoordinateDto(CoordinateDto coordinateDto) {
+        this.coordinateDto = coordinateDto;
+    }
+
+    public Long getTruckId() {
+        return truckId;
+    }
+
+    public void setTruckId(Long truckId) {
+        this.truckId = truckId;
+    }
+
+    public Long getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(Long driverId) {
+        this.driverId = driverId;
+    }
+
+    @Override
+    public String toString() {
+        return "TruckDriverWayDtoCreate{" +
+                "fuel=" + fuel +
+                ", distance=" + distance +
+                ", resultTime=" + resultTime +
+                ", coordinateDto=" + coordinateDto +
+                ", truckId=" + truckId +
+                ", driverId=" + driverId +
+                '}';
+    }
+}
